700c167c9875ff9e4c4651d51b9c4e4948aff668
Ben Cruwyscd <bencruwys@gmail.com>
2022-08-12 22:12:06 +0100

    Complete day 2 part 2 code. In the future, could be tidied.

--- day2/part2.py
+++ day2/part2.py
@@ -1,3 +1,21 @@
+ribbon = 0
+
 with open("day2/input.txt", "r") as file:
     for line in file:
         numbers = line.split("x")
+
+        # creates a list of the side lengths
+        sides = []
+        sides.append(int(numbers[0]))
+        sides.append(int(numbers[1]))
+        sides.append(int(numbers[2]))
+        
+        # for the bow 
+        ribbon += (sides[0] * sides[1] * sides[2])
+
+        sides.remove(max(sides))
+        
+        # for the main ribbon
+        ribbon += sum(sides) + sum(sides)
+
+print(ribbon)
